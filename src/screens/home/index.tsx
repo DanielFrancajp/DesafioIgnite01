7860534a638b3ea599e76891ad1920b2acca90c3
Daniel França <daniel.franca@secom.local>
2023-01-30 16:37:07 -0300

    Melhorando a estilização

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,50 +1,171 @@
-import React from "react";
+import React, { useState } from "react";
 import {
+    FlatList,
+    Image,
     Text,
     TextInput,
     TouchableOpacity,
     View,
+
 } from 'react-native'
+import IconAdd from 'react-native-vector-icons/AntDesign';
+import IconAcept from 'react-native-vector-icons/AntDesign';
+import IconDelete from "react-native-vector-icons/MaterialIcons";
+import IconCircle from "react-native-vector-icons/FontAwesome"
+
 import { styles } from './styles'
 
 export default function Home() {
-    return (
+
+
+
+    const data = [
+        {
+            id: "1",
+            description: "teste de exibicao de texto 1",
+            status: false,
+            delete: false,
+        },
+        {
+            id: "2",
+            description: "teste de exibicao de texto 2",
+            status: false,
+            delete: false
+        },
+        {
+            id: "3",
+            description: "teste de exibicao de texto 3",
+            status: false,
+            delete: false
+        },
+        {
+            id: "4",
+            description: "teste de exibicao de texto 4 ",
+            status: true,
+            delete: false
+        },
+        {
+            id: "5",
+            description: "teste de exibicao de texto 5 ",
+            status: true,
+            delete: false
+        },
+    ]
+
+       return (
 
 
         <View style={styles.container}>
 
+            <Image
+                source={require("../../../assets/Logo.png")}
+                style={styles.displayImage}
+            />
 
-            <Text style={styles.EventName1}> to</Text>
-            <Text style={styles.EventName2}> do</Text>
+            <View style={styles.container__body}>
+                <View style={styles.Form}>
+                    <TextInput
+                        style={styles.Input}
+                        placeholder={'Adicione uma nova tarefa'}
+                        placeholderTextColor="#808080"
+                    />
+                    <TouchableOpacity style={styles.Button}>
+                        <IconAcept
+                            name="pluscircleo"
+                            color="white"
+                            size={20}
+                        />
+
+                    </TouchableOpacity>
 
 
+                </View>
+
+                <View style={styles.container__names}>
+
+                    <View style={styles.TextCreate}>
+                        <Text style={styles.buttonCreate}> Criadas</Text>
+                        <View style={styles.borderNumberCreate}>
+                            <Text style={styles.color_text_number}>0</Text>
+                        </View>
+
+                    </View>
+
+                    <View style={styles.TextDone}>
+
+                        <Text style={styles.buttonDone}> Concluidas</Text>
+                        <View style={styles.borderNumberCreate}>
+                            <Text style={styles.color_text_number}>0</Text>
+                        </View>
+
+                    </View>
+                </View>
+
+                <FlatList
+                    data={data}
+                    extraData={data}
+                    ListEmptyComponent={
+                        <View style={styles.container__mid}>
 
 
-            <View style={styles.Form}>
-                <TextInput style={styles.Input}
-                    placeholder={'Adicione uma nova tarefa'}
-                    placeholderTextColor="#808080"
+                            <Image
+                                source={require("../../../assets/Clipboard.png")}
+                                style={styles.image__clipboard}
 
+                            />
+                            <Text style={styles.text1}> Você ainda não tem tarefas cadastradas</Text>
+                            <Text style={styles.text2}> Crie tarefas e organize seus itens a fazer</Text>
+
+                        </View>
+                    }
+                    renderItem={({ item }) => (
+                        <>
+                            {item.delete === false &&
+                                <View style={styles.containerFlat} >
+                                    <View style={styles.contentFlat} >
+                                        <View style={styles.buttonStatus} >
+                                            {item.status === false ?
+                                                <TouchableOpacity >
+                                                    <IconCircle
+                                                        name="circle-thin"
+                                                        color="purple"
+                                                        size={25}
+                                                    />
+
+                                                </TouchableOpacity>
+                                                :
+                                                <View >
+                                                    <IconAcept
+                                                        name="checkcircle"
+                                                        color="purple"
+                                                        size={20}
+                                                    />
+
+                                                </View>
+                                            }
+                                        </View>
+                                        <View style={styles.description} >
+                                            <Text style={styles.textDescription} >{item.description}</Text>
+                                        </View>
+                                        <View style={styles.buttonDelete} >
+                                            <TouchableOpacity >
+                                                <IconDelete
+                                                    name="delete-outline"
+                                                    color="red"
+                                                    size={25}
+                                                />
+
+                                            </TouchableOpacity>
+                                        </View>
+                                    </View>
+                                </View>
+                            }
+                        </>
+                    )}
+                    keyExtractor={item => item.id}
                 />
 
-                <TouchableOpacity style={styles.Button}>
-                    <Text style={styles.ButtonText}> + </Text>
-
-                </TouchableOpacity>
             </View>
-
-            <View style={styles.Space}>
-
-                <Text style={styles.Maids}> Criadas </Text>
-                <Text style={{paddingRight: 180}}>0</Text>
-                <Text style={styles.Concluded}> Concluídas</Text>
-                <Text >0</Text>
-
-            </View>
-
-
-
-
         </View>
 
 
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -5,62 +5,149 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#0D0D0D",
-        padding: 24,
+        alignItems: "center",
     },
-       EventName1: {
-        color: "#4EA8DE",
-        alignItems: 'center',
-        fontSize: 32,
-        fontWeight: 'bold'
+
+    displayImage: {
+        marginVertical: "20%",
+        marginBottom: "20%"
     },
-    EventName2: {
-        color: "#5E60CE",
-        fontSize: 32,
-        fontWeight: 'bold',
+
+    container__body: {
+        flex: 1,
+        width: "100%",
+        backgroundColor: "#1A1A1A",
 
     },
+
     Form: {
+        flexDirection: "row",
         width: '100%',
-        flexDirection: 'row',
-        marginTop: 36,
-        marginBottom: 42
-
+        marginHorizontal: 18,
+        marginTop: -28
     },
     Input: {
         flex: 1,
-        height: 56,
         backgroundColor: '#1F1E25',
-        borderRadius: 5,
         color: '#FDFCFE',
-        padding: 16,
-        fontSize: 16,
-        marginRight: 12
+        borderRadius: 5,
+        padding: 14,
+        paddingHorizontal: 16,
+        marginRight: 8,
     },
     Button: {
         width: 52,
         height: 52,
-        borderRadius: 5,
         backgroundColor: '#1E6F9F',
         alignItems: 'center',
         justifyContent: 'center',
+        borderRadius: 6,
+        marginRight: 32
     },
     ButtonText: {
         color: '#FFF',
         fontSize: 24,
 
     },
-    Space:{
-        flex:1,
-        flexDirection:'row',
-        justifyContent:'space-between'
+    container__names: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginHorizontal: 18,
+        marginTop: 25,
+        borderBottomWidth: 1,
+        borderBottomColor: '#333333',
+        paddingBottom: 20
     },
-     Maids: {
-        color: "#4EA8DE",
-        textAlign:'left'
-    },
-    Concluded: {
-        color: '#8284FA',
-        textAlign:'right'
+    buttonCreate: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        color: '#4EA8DE',
 
+    },
+    TextCreate: {
+        flexDirection: 'row',
+        alignItems: 'center',
+
+    },
+    color_text_number: {
+        color: '#FFF',
+    },
+    borderNumberCreate: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#333333',
+        paddingHorizontal: 10,
+        paddingVertical: 3,
+        marginLeft: 10,
+        borderRadius: 50
+    },
+    buttonDone: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        color: '#8284FA',
+
+    },
+    border__done: {
+        color: '#FFF',
+    },
+    TextDone: {
+        flexDirection: 'row',
+        alignItems: 'center',
+
+    },
+    container__mid: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginBottom: 250,
+    },
+    image__clipboard: {
+        marginTop: 40,
+        marginBottom: 20
+    },
+    text1: {
+        color: '#808080',
+        fontWeight: 'bold'
+    },
+    text2: {
+        color: '#808080'
+    },
+    containerFlat: {
+    marginHorizontal: 18,
+    padding: 10,
+    borderColor: "#333333",
+    borderRadius: 8,
+    borderWidth: 1,
+    marginVertical: 10,
+    },
+    contentFlat:{
+        width: "100%",
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+    },
+    buttonStatus:{
+        justifyContent: "center",
+        alignItems: "center",
+        width: "7%"
+    },
+    buttonDelete:{
+        width: "7%",
+        justifyContent: "center",
+        alignItems: "center",
+
+    },
+    textDescription:{
+        color:"#FFFFFF",
+        fontWeight: "bold",
+
+    },
+    description:{
+        alignItems: "center",
+        justifyContent: "center",
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderColor: "#333333",
+        width: "80%"
     }
+
 })
